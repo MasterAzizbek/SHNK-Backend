22f20591c6553e414fddee48a197a5e024e20265
Azizbek <alievazizbek93@gmail.com>
2024-05-23 01:10:10 +0500

    Updated

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ContactInfoModel, ContactModel
 
-# Register your models here.
+admin.site.register(ContactInfoModel)
+admin.site.register(ContactModel)
